be4cf2bc69a1595c2fec5e41ed52ff54964d56c2
Shin Ohno <ohnoshin@gmail.com>
2020-06-28 13:45:02 +0900

    Moved guard logic to Group

--- src/School/Group.php
+++ src/School/Group.php
@@ -26,6 +26,36 @@ class Group
 
     public function addPupil(Pupil $pupil) : void
     {
+        $this->guardAgainstTooManyPupils();
+        $this->guardAgainstDuplicatePupils($pupil);
+
         $this->pupils[] = $pupil;
+    }
+
+    /**
+     * @throws TooManyPupilsException
+     */
+    private function guardAgainstTooManyPupils(): void
+    {
+        if (count($this->getPupils()) >= 3) {
+            throw new TooManyPupilsException();
+        }
+    }
+
+    /**
+     * @param Pupil $pupil
+     * @throws PupilAlreadyInGroupException
+     */
+    private function guardAgainstDuplicatePupils(Pupil $pupil): void
+    {
+        $pupilAlreadyInGroup = false;
+        foreach ($this->getPupils() as $pupilInGroup) {
+            if ($pupilInGroup->getId() == $pupil->getId()) {
+                $pupilAlreadyInGroup = true;
+            }
+        }
+        if ($pupilAlreadyInGroup) {
+            throw new PupilAlreadyInGroupException();
+        }
     }
 }
