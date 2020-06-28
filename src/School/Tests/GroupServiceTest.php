33d17c11e10c9fea6b4b7cbf6b0e9a80a278150a
Shin Ohno <ohnoshin@gmail.com>
2020-06-28 12:53:52 +0900

    Added Tests fro GroupService

--- src/School/Tests/GroupServiceTest.php
+++ src/School/Tests/GroupServiceTest.php
@@ -14,21 +14,116 @@ use School\TooManyPupilsException;
 final class GroupServiceTest extends TestCase
 {
 
+    private const GROUP_ID = 123;
+
+    private const PUPIL_ID = 456;
+
+    /**
+     * @var GroupService
+     */
+    private GroupService $SUT;
+
+    /**
+     * @var GroupRepository
+     */
+    private $groupRepository;
+
+    /**
+     * @var PupilRepository
+     */
+    private $pupilRepository;
+
+    /**
+     * @var Group
+     */
+    private Group $group;
+
+    /**
+     * @var Pupil
+     */
+    private Pupil $pupil;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->group = new Group(self::GROUP_ID);
+        $this->pupil = new Pupil(self::PUPIL_ID);
+
+        $this->groupRepository = $this->createMock(GroupRepository::class);
+        $this->pupilRepository = $this->createMock(PupilRepository::class);
+
+        $this->SUT = new GroupService($this->groupRepository, $this->pupilRepository);
+    }
+
     /**
      * @test
      */
-    public function it_should_()
+    public function it_should_enlist_pupil_in_group()
     {
-        $groupRepository = $this->getMockBuilder('School\GroupRepository')
-          ->getMock();
-        $pupilRepository = $this->getMockBuilder('School\PupilRepository')
-          ->getMock();
+        $this->assertEmpty($this->group->getPupils());
 
-        $group = new GroupService($groupRepository, $pupilRepository);
+        $this->expectThatAGroupIsFound();
+        $this->expectThatAPupilIsFound();
+        $this->expectThatGroupIsPersisted();
 
-        $group->add(123, 234);
+        $this->SUT->add(self::GROUP_ID, self::PUPIL_ID);
+        $this->assertEquals([$this->pupil], $this->group->getPupils());
+    }
 
-        $this->assertTrue(true);
+    /**
+     * @test
+     */
+    public function it_should_disallow_more_than_three_pupils_in_group()
+    {
+        $this->group->addPupil(new Pupil(1));
+        $this->group->addPupil(new Pupil(2));
+        $this->group->addPupil(new Pupil(3));
+
+        $this->expectThatAGroupIsFound();
+        $this->expectThatAPupilIsFound();
+
+        $this->expectException(TooManyPupilsException::class);
+        $this->SUT->add(self::GROUP_ID, self::PUPIL_ID);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_disallow_duplicate_pupils()
+    {
+        $this->group->addPupil($this->pupil);
+
+        $this->expectThatAGroupIsFound();
+        $this->expectThatAPupilIsFound();
+
+        $this->expectException(PupilAlreadyInGroupException::class);
+        $this->SUT->add(self::GROUP_ID, self::PUPIL_ID);
+
+    }
+
+    private function expectThatAGroupIsFound(): void
+    {
+        $this->groupRepository
+          ->expects($this->once())
+          ->method('find')
+          ->with(self::GROUP_ID)
+          ->will($this->returnValue($this->group));
+    }
+
+    private function expectThatAPupilIsFound(): void
+    {
+        $this->pupilRepository
+          ->expects($this->once())
+          ->method('find')
+          ->with(self::PUPIL_ID)
+          ->will($this->returnValue($this->pupil));
+    }
+
+    private function expectThatGroupIsPersisted(): void
+    {
+        $this->groupRepository->expects($this->once())
+          ->method('persist')
+          ->with($this->group);
     }
 
 }
