dd03cf888a0c63c875fd0f65999af6674bb9696b
Shin Ohno <ohnoshin@gmail.com>
2020-06-28 13:53:23 +0900

    Removed extracted guard logic from GroupService

--- src/School/GroupService.php
+++ src/School/GroupService.php
@@ -19,42 +19,9 @@ class GroupService
     public function enlistPupilInGroup($groupId, $pupilId)
     {
         $group = $this->groupRepository->find($groupId);
-
         $pupil = $this->pupilRepository->find($pupilId);
-
-        $this->guardAgainstTooManyPupils($group);
-        $this->guardAgainstDuplicatePupils($group, $pupil);
 
         $group->addPupil($pupil);
         $this->groupRepository->persist($group);
-    }
-
-    /**
-     * @param Group $group
-     * @throws TooManyPupilsException
-     */
-    private function guardAgainstTooManyPupils(Group $group): void
-    {
-        if (count($group->getPupils()) >= 3) {
-            throw new TooManyPupilsException();
-        }
-    }
-
-    /**
-     * @param Group $group
-     * @param Pupil $pupil
-     * @throws PupilAlreadyInGroupException
-     */
-    private function guardAgainstDuplicatePupils(Group $group, Pupil $pupil): void
-    {
-        $pupilAlreadyInGroup = false;
-        foreach ($group->getPupils() as $pupilInGroup) {
-            if ($pupilInGroup->getId() == $pupil->getId()) {
-                $pupilAlreadyInGroup = true;
-            }
-        }
-        if ($pupilAlreadyInGroup) {
-            throw new PupilAlreadyInGroupException();
-        }
     }
 }
